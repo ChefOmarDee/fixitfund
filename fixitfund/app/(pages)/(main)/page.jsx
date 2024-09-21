382f2035f60f8d5588da19e9e4621b374643cd35
Bebo561 <soto.carlodavid@gmail.com>
2024-09-21 03:12:54 -0400

    Completed images and loading screen

--- fixitfund/app/(pages)/(main)/page.jsx
+++ fixitfund/app/(pages)/(main)/page.jsx
@@ -1,5 +1,7 @@
 "use client"
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import ProgressBar from '@ramonak/react-progress-bar';
 
 export default function Home() {
   const testingData = [
@@ -12,12 +14,13 @@ export default function Home() {
       Cost: "10000",
       Donated: "100",
       Status: "Open",
-      PictureUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fcute-cat&psig=AOvVaw0AsRKsCeA-rRrEhI4lcCpR&ust=1726985991950000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPjrk6ay04gDFQAAAAAdAAAAABAE",
+      PictureUrl: "https://as2.ftcdn.net/v2/jpg/02/66/72/41/1000_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpg",
       Tags: "Schlawg Clone FRFR"
-    }
+    } 
   ]
   let [projectArray, setProjects] = useState([]);
   let [loading, setLoading] = useState(false);
+  const router = useRouter();
 
   const fetchProjects = async () => {
     setLoading(true);
@@ -33,9 +36,13 @@ export default function Home() {
       setLoading(false);
     }
   };
+
+  const redirectToProject = (projectId) => {
+    router.push(`/project/${projectId}`);
+  }
   
   useEffect(() => {
-    fetchProjects();
+    //fetchProjects();
   }, [fetchProjects])
 
   return (
@@ -44,10 +51,31 @@ export default function Home() {
         <h1 className={'text-white text-[60px] font-bold'}>Fix-It-Fund</h1> 
         <h3 className ={'text-white text-[20px] font-medium'} >Your one stop shop for improving your community</h3>
       </div>
-      <div className={'min-h-[50vh] max-h-[100%] flex-grow overflow-y-auto'}>
+      <div className = {'bg-[#FFFAF1] '}>
         <h1 className='text-center font-bold text-[40px]'>Home</h1>
-
       </div>
+      {loading &&
+        <div className={'h-[100%] w-[100%] text-[100px] bg-[#FFFAF1] flex text-center justify-center align-center'}>
+          <h1 className="mt-[10vh] font-bold">loading...</h1>
+        </div>
+      }
+      {!loading &&
+      <div className={'min-h-[100%] bg-[#FFFAF1] hover:cursor-pointer max-md:grid-cols-1 grid grid-cols-3 overflow-y-auto'}>
+          {projectArray.length !== 0 &&  projectArray.map((project) => (
+          <div key={project.ProjectId} onClick={() => redirectToProject(project.ProjectId)} className="w-[25vw] rounded-xl mx-auto h-[40vh] hover:bg-gray-300 transition-colors duration-300 max-md:w-[85vw] max-md:h-[45vh] mt-[2vh] overflow-hidden bg-gray-200">
+            <img 
+              src={project.PictureUrl} 
+              alt={project.Title} 
+              className="w-full h-[75%] object-cover" 
+            />
+            <h3 className="text-black font-bold text-xl ml-[10px]">{project.Title}</h3>
+            <h4 className="font-medium text-black text-md ml-[10px]">{project.Description}</h4>
+            <ProgressBar bgColor='green' width={'90%'} margin='0 0 0 10px' completed={(parseInt(project.Donated)/parseInt(project.Cost)) * 100} customLabelStyles={{ paddingLeft: '10px'}}/>
+            <h4 className="text-sm text-black font-light ml-[10px]">Amount Donated: {(parseInt(project.Donated)/parseInt(project.Cost)) * 100}%</h4>
+          </div>
+        ))}
+      </div>
+    }
     </div>
   );
 }
