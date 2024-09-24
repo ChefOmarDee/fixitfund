7aaceb790b1c1c0ee1779a5fd0c6ace32227c351
ChefOmarDee <chefomardee@gmail.com>
2024-09-23 21:21:58 -0400

    fixed caching

--- fixitfund/app/api/getallprojectdetails/route.js
+++ fixitfund/app/api/getallprojectdetails/route.js
@@ -1,12 +1,25 @@
-import { getallprojectdetails } from '../../_lib/mongo/utils/getallprojectdetails';
-import { NextResponse } from 'next/server';
+import { getallprojectdetails } from "../../_lib/mongo/utils/getallprojectdetails";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function GET(req) {
-    try {
-        const result = await getallprojectdetails();
-        return NextResponse.json({data: result}, {status: 200});
-    } catch (error) {
-        console.error("Error in API route:", error);
-        return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
-    }
+	try {
+		const result = await getallprojectdetails();
+
+		const response = NextResponse.json({ data: result }, { status: 200 });
+
+		response.headers.set("Cache-Control", "no-store, max-age=0");
+		response.headers.set("Pragma", "no-cache");
+		response.headers.set("Expires", "0");
+
+		return response;
+	} catch (error) {
+		console.error("Error in API route:", error);
+		return NextResponse.json(
+			{ error: "Internal server error" },
+			{ status: 500 }
+		);
+	}
 }
